6f23eda3e1003f8de8ffcc105a28c3e719157115
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-09-05 12:41:11 +0300

    Objects Methods

---
+++ JAVASCRIPT/Objects Methods/exercise.js
@@ -0,0 +1,10 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.keys
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
