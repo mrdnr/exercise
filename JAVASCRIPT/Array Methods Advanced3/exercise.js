a88a7d97b5f78b13b8a09091665a7d40d5607461
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-09-05 15:10:11 +0300

    Array Methods Advanced3

---
+++ JAVASCRIPT/Array Methods Advanced3/exercise.js
@@ -0,0 +1,18 @@
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const totalAge = persons.reduce((accumulator,person) => accumulator + person.age, 0)
+const average = totalAge / persons.length
+console.log(totalAge);
+console.log(average);
