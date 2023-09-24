4e73db28c75124862b30ed5568bbedae9dfbe023
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-09-24 22:38:18 +0300

    getter setter

--- JAVASCRIPT/Getters and Setters/exercise.js
+++ JAVASCRIPT/Getters and Setters/exercise.js
@@ -1,18 +1,42 @@
 class Person {
-  constructor(firstName,lastName,age){
-    this.firstName=firstName;
-    this.lastName=lastName;
-    this.age=age;
+  constructor(firstName, lastName, age) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._age = age;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(newFirstName) {
+    this._firstName = newFirstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(newLastName) {
+    this._lastName = newLastName;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    this._age = newAge;
+  }
+
+  get fullName(){
+    return `${this._firstName} ${this._lastName} ${this._age}`;
   }
 }
 
-Object.defineProperty(Person,`fullName`,{
-  
-})
-
-const person = new Person('Mario', 'Rossi', 25);
+const person = new Person("Mario", "Rossi", 25);
 console.log(person.fullName);
 
-person.firstName = 'Maria';
-person.lastName = 'Verdi';
+person.firstName = "Maria";
+person.lastName = "Verdi";
 console.log(person.fullName);
