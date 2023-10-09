3b683179f3ee3d9d406e730e819e2cae3d27b6e3
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-10-09 11:12:53 +0300

    js2

--- JAVASCRIPT/Local Storage2/exercise.js
+++ JAVASCRIPT/Local Storage2/exercise.js
@@ -3,3 +3,9 @@ const user = {
   name: "John",
   age: 25,
 };
+
+localStorage.setItem("user", JSON.stringify(user));
+
+const saveData = JSON.parse(localStorage.getItem("user"));
+
+console.log(saveData);
