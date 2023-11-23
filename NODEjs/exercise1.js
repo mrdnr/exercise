978f5bf732cef46d0090f562257398c85af0b0f0
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-11-23 12:37:04 +0300

    second function

--- NODEjs/exercise1.js
+++ NODEjs/exercise1.js
@@ -4,3 +4,9 @@ const num2= 12;
 const sum = num1 + num2;
 
 console.log(`The sum result : ${sum}`);
+
+function total(number1, number2) {
+    return number1 + number2
+};
+
+console.log("Result:", total(3,9))
